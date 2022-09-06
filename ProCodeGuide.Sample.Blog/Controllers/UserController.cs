7aa99fbfc45061f0f551db389b1d0b6266fa3f44
Danielic <Danielic@bynetsoft.com>
2022-09-06 09:46:41 +0300

    added claims

---
+++ ProCodeGuide.Sample.Blog/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProCodeGuide.Sample.Blog.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+       
+        private readonly ILogger<UserController> _logger;
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Claims()
+        {
+            return View();
+        }
+    }
+}
