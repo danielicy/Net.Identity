47e4646f9163fe2b7de1abdc8a6e64b8a1f108ae
Danielicy <danielicy@hotmail.com>
2022-09-09 10:58:34 +0300

    OpenIdConnect

---
+++ OpenIDConnect/OpenIDConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OpenIDConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
